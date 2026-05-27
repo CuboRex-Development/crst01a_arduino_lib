061073b6d5f411a71a0d775421e99d8dfa1d85c4
Ogawa26yf <79568269+Ogawa26yf@users.noreply.github.com>
2026-05-27 16:15:56 +0900

    サンプルの修正
    不要な処理の削除
    コメントの追加

--- examples/change_battery_voltage_limits/change_battery_voltage_limits.ino
+++ examples/change_battery_voltage_limits/change_battery_voltage_limits.ino
@@ -4,19 +4,17 @@
 
 void setup() {
 	
-	Serial.begin(115200);
-	
 	// 初期化処理
 	cugoCommon.Init();
 }
 
 void loop() {
 
-	cugoCommon.SetVoltage(20,33);	// 18V以下、32V以上で電圧異常と判断
+	cugoCommon.SetVoltage(18,32);	// 18V以下、32V以上で電圧異常と判断する設定
 	
-	cugoCommon.SaveParamReq();				// フラッシュメモリに保存
+	cugoCommon.SaveParamReq();		// フラッシュメモリに保存
 	
-	// 処理終了
+	// 処理終了を知らせるLED点滅
 	while(1){
 		digitalWrite(LED_BUILTIN, HIGH);
 		delay(1000);
--- examples/change_max_speed/change_max_speed.ino
+++ examples/change_max_speed/change_max_speed.ino
@@ -5,8 +5,6 @@
 
 void setup() {
 	
-	Serial.begin(115200);
-	
 	// 初期化処理
 	cugoDiffDriveCtrl.Init();
 	cugoCommon.Init();
@@ -14,12 +12,12 @@ void setup() {
 
 void loop() {
 
-//	cugoDiffDriveCtrl.SetMaxSpeed(1000,1500);	// 3.6km/h
-	cugoDiffDriveCtrl.SetMaxSpeed(1222,1500);	// 4.4km/h
+//	cugoDiffDriveCtrl.SetMaxSpeed(1000,1500);	// 走行速度を1.0m/s(3.6km/h)と1.5rad/sに設定
+	cugoDiffDriveCtrl.SetMaxSpeed(1222,1500);	// 走行速度を1.222m/s(4.4km/h)と1.5rad/sに設定
 	
 	cugoCommon.SaveParamReq();				// フラッシュメモリに保存
 	
-	// 処理終了
+	// 処理終了を知らせるLED点滅
 	while(1){
 		digitalWrite(LED_BUILTIN, HIGH);
 		delay(1000);
