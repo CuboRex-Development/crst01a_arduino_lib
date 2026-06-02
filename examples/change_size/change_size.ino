750e218c05a0d4543b23d6d1b3cf6cfd2415138c
Ogawa26yf <79568269+Ogawa26yf@users.noreply.github.com>
2026-06-02 16:29:53 +0900

    サンプルスケッチの修正

--- examples/change_size/change_size.ino
+++ examples/change_size/change_size.ino
@@ -15,7 +15,7 @@ void loop() {
 	cugoDiffDriveCtrl.SetKinematics(0.07716,0.07716,0.1144064068,0.1144064068,0.38);	// V4.5デフォルト設定。トレッド幅は0.38m
 //	cugoDiffDriveCtrl.SetKinematics(0.07716,0.07716,0.1144064068,0.1144064068,0.5);		// トレッド幅を0.5mに設定する場合
 	
-//	cugoCommon.SaveParamReq();				// フラッシュメモリに保存
+	cugoCommon.SaveParamReq();				// フラッシュメモリに保存
 	
 	// 処理終了を知らせるLED点灯
 	cugoCommon.LedPrint(false);
